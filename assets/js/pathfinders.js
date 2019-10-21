56a6d3fd265665ff1f2c94856589e7421b6f3ba9
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2019-10-21 02:58:26 -0300

    Form validation

--- assets/js/pathfinders.js
+++ assets/js/pathfinders.js
@@ -19,5 +19,55 @@
             });
         });
     });
+    // Validation
+    var $form = document.forms[0];
+    $form.addEventListener('submit', function(event){
+        event.preventDefault();
+        console.log(event);
+        var name = $form['name'];
+        console.log(name);
+        var email = $form['email'];
+        var phone = $form['phone'];
+        var message = $form['message'];
+
+        if(name.value == "" ){    
+            name.focus();
+            return;
+        }
+
+        if(email.value == "" ){
+
+            email.focus();
+            return;
+        }
+        if(!validateEmail(email.value)){
+            console.log(email.value);
+            email.focus();
+            return;
+        }
+        
+
+        if(phone.value == "" ){
+            
+            phone.focus();
+            return;
+        }
+        // Sydney phone validation
+        if(message.value == "" ){
+            
+            message.focus();
+            return;
+        }
+
+        //Call Api to email: paul.steven.conyngham@gmail.com
+
+        $form.reset();
+        
+    });
+    
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
 
 }( $ ));
